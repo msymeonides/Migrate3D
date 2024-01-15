669a1e1d1c4d48c92708f11068ee9d64f50b0c14
emynar <emynar@uvm.edu>
2024-01-14 20:59:02 -0700

    Fixing overall_medians

--- overall_medians.py
+++ overall_medians.py
@@ -1,14 +1,14 @@
 import statistics
 
 
-def overall_medians(cell, df, cols_angles, cols_euclidean, parent_id):
+def overall_medians(cell, df_all_calcs, cols_angles, cols_euclidean):
     list_of_angle_medians = []
     list_of_euclidean_medians = []
     single_euclidean = []
     single_angle = []
 
     for col_ in cols_angles:
-        angle_median = list(df.loc[df[parent_id] == cell, col_])
+        angle_median = list(df_all_calcs.loc[df_all_calcs['Cell ID'] == cell, col_])
         angle_median = [x for x in angle_median if x is not None and x != 0]
         if len(angle_median) > 2:
             angle_median = statistics.median(angle_median)
@@ -21,7 +21,7 @@ def overall_medians(cell, df, cols_angles, cols_euclidean, parent_id):
             pass
 
     for cols_ in cols_euclidean:
-        euclidean_median = df.loc[df[parent_id] == cell, cols_]
+        euclidean_median = df_all_calcs.loc[df_all_calcs['Cell ID'] == cell, cols_]
         euclidean_median = [x for x in euclidean_median if x is not None and x != 0]
         if len(euclidean_median) > 2:
             euclidean_median = statistics.median(euclidean_median)
