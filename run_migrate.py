5a5f85c683d2b878c87affd96bc45e31e417ea05
msymeonides <msymeoni@uvm.edu>
2025-05-05 11:49:45 -0400

    Revert "Dash App live updated and non functional stop button"
    This reverts commit 6ba594024a866b04e8984925aec53cbb9ae719d9.

--- run_migrate.py
+++ run_migrate.py
@@ -1,41 +1,87 @@
 import pandas as pd
+import numpy as np
 import os
 from formatting import multi_tracking, adjust_2D, interpolate_lazy
+from calculations import calculations
 import time as tempo
 from summary_sheet import summary_sheet
 from attract import attract
+import parallel_contacts
+from summarize_contacts import summarize_contacts
 
 
+def migrate3D(parent_id, time_for, x_for, y_for, z_for, timelapse_interval, arrest_limit, moving, contact_length,
+              arrested,
+              tau_msd, tau_euclid, formatting_options, savefile, segments_file_name, tracks_file, parent_id2,
+              category_col_name, parameters, pca_filter):
+    bigtic = tempo.time()
+    print(parent_id, time_for, x_for, y_for, z_for)
+    # update param dict
+    parameters['savefile'] = savefile
+    parameters['x_col_name'] = x_for
+    parameters['object_id_col_name'] = parent_id
+    parameters['time_col_name'] = time_for
+    parameters['x_col_name'] = x_for
+    parameters['y_col_name'] = y_for
+    parameters['z_col_name'] = z_for
+    parameters['timelapse'] = timelapse_interval
+    parameters['arrest_limit'] = arrest_limit
+    parameters['moving'] = moving
+    parameters['contact_length'] = contact_length
+    parameters['arrested'] = arrested
+    parameters['tau_msd'] = tau_msd
+    parameters['tau_euclid'] = tau_euclid
+    parameters['pca_filter'] = pca_filter
 
+    # formatting options for param update
+    if formatting_options is None:
+        pass
     else:
+        if 'Multitrack' in formatting_options:
+            parameters['multi_track'] = True
+        if 'Two-dimensional' in formatting_options:
+            parameters['two_dim'] = True
+        if 'Interpolate' in formatting_options:
+            parameters['interpolate'] = True
+        if 'Verbose' in formatting_options:
+            parameters['verbose'] = True
+        if 'Contacts' in formatting_options:
+            parameters['contact'] = True
+
+    # check if tracks used at all and update accordingly
+    if 'Enter your category .csv file here by clicking or dropping:' in tracks_file:
+        pass
     else:
+        parameters['infile_tracks'] = True
+        parameters['object_id_2_col'] = parent_id2
+        parameters['category_col'] = category_col_name
 
+    # Build the content folder path
+    content_folder = os.path.join(os.getcwd())
 
+    # Specify the file name you want to reference
+    segments_file_name = segments_file_name
+
+    # Construct the full file path
+    infile_path = os.path.join(content_folder, segments_file_name)
+
+    # Assign the file path to parameters (update key as needed)
+    parameters['infile_segments'] = infile_path
 
     infile_name = parameters['infile_segments']
     infile_segments = pd.read_csv(infile_name, sep=',')
     df_infile = pd.DataFrame(infile_segments)
 
-                # Detect timelapse interval
-                if parameters['timelapse'] != 0:
-                    timelapse_interval = round((parameters['timelapse']), 3)
-                else:
-                    time_data = df_infile[time_for].values
-                    time_diffs = np.diff(time_data)
-                    print(time_diffs)
-                    timelapse_interval = mode(time_diffs).mode[0]
-                    print(timelapse_interval)
-
-                # Detect 2D data
-                if (z_for == '') | (z_for not in df_infile.columns):
-                    print('2D dataset detected. If your dataset is 3D, check your Z column name.')
-
+    # Check if the segments file column names match
+    # todo: Not needed now that columns are dropdowns directly from import file but here for y'all to decide
+    expected_columns = [parent_id, time_for, x_for, y_for, z_for]
     for col in expected_columns:
         if col not in df_infile.columns:
             print(f"Error: Column '{col}' not found in Segments input file. Please fix the column names.")
             return
             # Check if the column names match in infile_tracks
     if parameters['infile_tracks']:
+        df_tracks = pd.read_csv(tracks_file, sep=',')
         object_id_2 = parameters['object_id_2_col']
         category_col_name = parameters['category_col']
         expected_track_columns = [object_id_2, category_col_name]
@@ -44,6 +90,7 @@ from attract import attract
                 print(f"Error: Column '{col}' not found in Tracks input file. Please fix the column names.")
                 return
 
+    if z_for is None:
         df_infile[z_for] = 0
 
     input_data_list = []
@@ -59,9 +106,14 @@ from attract import attract
     arr_segments = np.array(input_data_list)
 
     settings = {'Segments file': [os.path.basename(infile_name).split('/')[-1]],
+                'Categories file': [os.path.basename(tracks_file).split('/')[-1]],
                 'Timelapse Interval': [timelapse_interval], 'Arrest Limit': [arrest_limit],
+                'Min. TP Moving': parameters['moving'], 'Max. Contact Length': [contact_length],
                 'Arrested': [arrested], 'Tau (MSD)': [parameters['tau_msd']], 'Tau (Euclid/Angle)': [tau_euclid],
+                'Interpolation': parameters['interpolate'], 'Multitracking': [parameters['multi_track']], 'Adjust to 2D': parameters['two_dim'],
+                'Contacts': parameters['contact']}
     df_settings = pd.DataFrame(data=settings)
+
 
     # Sort segments array by time and object ID
     arr_segments = arr_segments[arr_segments[:, 1].argsort()]
@@ -90,10 +142,12 @@ from attract import attract
     print('Calculating migration parameters...')
     # Perform calculations on each unique object
     all_calcs = []
+
     for object in unique_objects:
         object_data = arr_segments[arr_segments[:, 0] == object, :]
         object_id = object_data[0, 0]
         df_calcs = calculations(object_data, tau_euclid, object_id, parameters)
+        # dpg.set_value('pbar', p_bar_increase)
         all_calcs.append(df_calcs)
     df_all_calcs = pd.concat(all_calcs)
     mapping = {0: None}
@@ -107,6 +161,7 @@ from attract import attract
     category_col_name = parameters['category_col']
 
     if parameters['infile_tracks']:
+        track_df = pd.DataFrame(pd.read_csv(tracks_file))
         for row in track_df.index:
             object_id2 = track_df[object_id_2][row]
             category = track_df[category_col_name][row]
@@ -116,24 +171,50 @@ from attract import attract
         arr_tracks = np.zeros_like(arr_segments)
 
     # Create summary sheet of calculations
+    df_sum, time_interval, df_single, df_msd, df_msd_sum_all, df_msd_sum_cat, df_pca = summary_sheet(arr_segments,
                                                                                                      df_all_calcs,
                                                                                                      unique_objects,
+                                                                                                     parameters[
+                                                                                                         'tau_msd'],
                                                                                                      parameters,
+                                                                                                     arr_tracks,
+                                                                                                     savefile)
     tic = tempo.time()
+
+    if parameters['infile_tracks']:
         print('Detecting attractors...')
         # Create a mapping from object IDs to cell types
         cell_types = dict(zip(track_df[parameters['object_id_2_col']], track_df[parameters['category_col']]))
         attract(unique_objects, arr_segments, cell_types, df_all_calcs, savefile)
         toc = tempo.time()
         print('...Attractors done in {:.0f} seconds.'.format(int(round((toc - tic), 1))))
-
+    else:
+        pass
 
     # Check if contacts parameter is true and if so call contacts functions
+    if parameters['contact'] is False:
         pass
     else:
         tic = tempo.time()
         print('Detecting contacts...')
+        # Extract the timepoints from arr_segments (column index 1) and get unique values
+        unique_timepoints = np.unique(arr_segments[:, 1])
+        # Then pass unique_timepoints to parallel_contacts.main instead of unique_objects
+        df_contacts, df_no_daughter, df_no_dead_ = parallel_contacts.main(
+            unique_timepoints,  # using timepoints for chunking
+            arr_segments,
+            parameters['contact_length'],
+            df_sum,
+            parameters['arrested'],
+            timelapse_interval
+        )
+        if not df_no_dead_.empty:
+            df_contact_summary = summarize_contacts(df_no_dead_, timelapse_interval)
+            print(f"Contact summary created with {len(df_contact_summary)} rows.")
         else:
+            df_contact_summary = pd.DataFrame()
+            print("No valid contacts detected; skipping summary processing.")
+
         toc = tempo.time()
         print('...Contacts done in {:.0f} seconds.'.format(int(round((toc - tic), 1))))
 
@@ -147,7 +228,6 @@ from attract import attract
 
     # restore zeros for Arrest Coefficient
     df_sum['Arrest Coefficient'] = df_sum.loc[:, 'Arrest Coefficient'].replace((np.nan, ' '), (0, 0))
-
 
     # Create file path
     savepath = savefile + '.xlsx'
@@ -168,6 +248,7 @@ from attract import attract
                 df_msd_sum_cat.to_excel(workbook, sheet_name='MSD Per Category', index=True)
             else:
                 pass
+
     else:  # Save only summary data to Excel file
         with pd.ExcelWriter(savepath, engine='xlsxwriter', engine_kwargs={'options': {'zip64': True}}) as workbook:
             df_settings.to_excel(workbook, sheet_name='Settings', index=False)
@@ -181,6 +262,7 @@ from attract import attract
                 pass
 
         # If contacts were detected, save contacts to separate Excel file
+        if parameters['contact'] is False:
             pass
         else:
             print('Saving contacts output to ' + savecontacts + '...')
@@ -201,4 +283,4 @@ from attract import attract
         else:
             print('Total time taken = {:.1f} minutes.'.format(total_time_min))
 
-
+    return df_segments, df_sum, df_pca
